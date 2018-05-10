f0fd672bf74b8ba54e0ada5ee282763a651e3a9f
Tyler Stahl <tyler@volantweb.com>
2018-05-10 16:48:48 -0400

    initial build

---
+++ index.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="A portfolio of featured website projects from Tyler Stahl, web professional">
+    <meta name="author" content="Tyler Stahl">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Portfolio: Tyler Stahl</title>
+
+    <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400" rel="stylesheet">
+
+    <!-- CSS stylesheets -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <header id="heading" class="container">
+      <div  class="row">
+        <div class="col-md-2">
+          <img src="images/volant_logo copy.png" class="img-fluid" alt="Volant Web Design logo">
+        </div>
+        <div class="col-md-10 heading-title">
+          <h1>Tyler Stahl</h1>
+          <p>Design Director, Volant Web Design</p>
+        </div>
+      </div>
+    </header>
+    
+    <main class="container">
+      <div class="row">
+        <div class="banner col-md-12">
+          <img src="images/take_flight.jpg" class="img-fluid" alt="canada geese in flight">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <h3 id="sub-heading">Featured Work</h3>
+        </div>
+        <div class="featured col-md-4">
+          <img src="images/client-drdevlyn.png" class="img-fluid" alt="">
+          <h2 class="featured-title">Dr. Devlyn</h2>
+          <p class="featured-address">https://drdevlyn.com/</p>
+        </div>
+        <div class="featured col-md-4">
+          <img src="images/client-ccr.png" class="img-fluid" alt="">
+          <h2 class="featured-title">Christ Church Ridgewood</h2>
+          <p class="featured-address">http://christchurchridgewood.org/</p>
+        </div>
+        <div class="featured col-md-4">
+          <img src="images/client-arlene.png" class="img-fluid" alt="">
+          <h2 class="featured-title">Growing Up Strong</h2>
+          <p class="featured-address">https://growingupstrong.net/</p>
+        </div>
+      </div>
+
+    </main>
+
+    <footer class="container">
+      <div class="row">
+        <div class="col-md-12 text-center">
+          <div class="text-muted">
+              &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://volantweb.com/" target="_blank" style="color: #868e96">Volant Web Design</a> 
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="js/jquery.min.js"></script> 
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+  </body>
+</html>
